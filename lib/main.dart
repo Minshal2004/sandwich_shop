c87d4db145f64d36b61cfd2fc93a389cd6b3b34e
Minshal2004 <minshal1786@gmail.com>
2025-11-14 14:48:20 +0000

    Added pricingrepository file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum BreadType { white, wheat, wholemeal }
+
 void main() {
   runApp(const App());
 }
---
+++ lib/repositories/PricingRepository.dart
@@ -0,0 +1,14 @@
+class PricingRepository {
+  final int quantity;
+  final bool isFootlong;
+
+  PricingRepository({
+    required this.quantity,
+    required this.isFootlong,
+  });
+
+  double getTotalPrice() {
+    final pricePerSandwich = isFootlong ? 11.0 : 7.0;
+    return pricePerSandwich * quantity;
+  }
+}
